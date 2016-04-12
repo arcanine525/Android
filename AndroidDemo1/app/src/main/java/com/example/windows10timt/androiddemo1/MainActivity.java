0288750235e61d7ba6f6c8bc1630698a35c0136c
arcanine525 <arcanine1450222@gmail.com>
2016-04-12 22:47:52 +0700

    Commit UI Demo

---
+++ AndroidDemo1/app/src/main/java/com/example/windows10timt/androiddemo1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.windows10timt.androiddemo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText password, userName;
+    Button logInBtn, signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        password = (EditText) findViewById(R.id.password);
+        userName = (EditText) findViewById(R.id.userName);
+        logInBtn = (Button) findViewById(R.id.logInBtn);
+        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+
+        View.OnClickListener click = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = v.getId();
+                String name=userName.getText().toString();
+                String pass=password.getText().toString();
+                switch (id){
+                    case R.id.logInBtn:
+                        break;
+                    case R.id.signUpBtn:
+                        break;
+
+                }
+            }
+
+
+        };
+
+
+    }
+}
